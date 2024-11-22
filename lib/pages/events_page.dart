263d43964e81d9374b36d7eb9101ddb66733fa00
ericlillemann <eric.lillemann@gmail.com>
2024-11-22 21:23:57 +0100

    Add BottomNavigationBar

---
+++ lib/pages/events_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class EventsPage extends StatelessWidget {
+  const EventsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Events page');
+  }
+}
---
+++ lib/pages/journal_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class JournalPage extends StatelessWidget {
+  const JournalPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Journal page');
+  }
+}
