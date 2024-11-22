39c6d8a877f01aee38fc967153b003592eca1351
ericlillemann <eric.lillemann@gmail.com>
2024-11-22 21:54:27 +0100

    Implement profile page

--- lib/pages/events_page.dart
+++ lib/pages/events_page.dart
@@ -1,10 +1,20 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class EventsPage extends StatelessWidget {
-  const EventsPage({super.key});
+  const EventsPage({super.key, required this.title});
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Events page');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text('No events found :('),
+      ),
+    );
   }
 }
--- lib/pages/journal_page.dart
+++ lib/pages/journal_page.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class JournalPage extends StatelessWidget {
-  const JournalPage({super.key});
+  const JournalPage({super.key, required this.title});
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Journal page');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text('No journals found :('),
+      ),
+    );
   }
 }
