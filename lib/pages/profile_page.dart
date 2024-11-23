ed37d84a6e40ace4fffed1134e7bdb6b7d4bf277
Yannis Matezki <git@yannismate.de>
2024-11-24 00:13:49 +0100

    WIP

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -12,9 +12,31 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
+class AchievementData {
+  AchievementData({required this.name, required this.imagePath, required this.icon});
+  final String name;
+  final String imagePath;
+  final IconData icon;
+}
+
 class _ProfilePageState extends State<ProfilePage> {
   String? user_email = FirebaseAuth.instance.currentUser!.email;
   String? username = FirebaseAuth.instance.currentUser!.displayName;
+
+  final Map<String, AchievementData> achievements = {
+    "STARTER": AchievementData(name: "Starter", imagePath: "lvl1_false.png", icon: Icons.abc),
+    /*"THE_JOURNEY": false,
+    "HI_FOLKS": false,
+    "REGULAR": false,
+    "PROFESSIONAL_SOCIALIZER": false,
+    "HOST": false,
+    "REAL_ORGANIZER": false,
+    "BARTENDER": false,
+    "LOCAL_HERO": false,
+    "ON_FIRE": false,
+    "UNSTOPPABLE": false,
+    "VERIFIED": false,*/
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -63,6 +85,49 @@ class _ProfilePageState extends State<ProfilePage> {
             ),
             SizedBox(height: 32),
 
+            GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, // 3 columns
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+            ), itemCount: achievements.length, itemBuilder: (context, index) {
+              final achievement = achievements[achievements.keys.elementAt(index)]!;
+              return Column(
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      // Background image
+                      Container(
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/img/" + achievement.imagePath),
+                            fit: BoxFit.cover,
+                          ),
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      // Overlay icon
+                      Icon(
+                        achievement.icon,
+                        size: 40,
+                        color: Colors.white.withOpacity(0.8),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  // Achievement name
+                  Text(
+                    achievement.name,
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              );
+            },
+            )
             // Weitere Details
           ],
         ),
